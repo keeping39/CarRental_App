b24577037cc02aac854cf5fd5d8c1daba72c9d4c
Ovidiu Piorescu <djenergydj@yahoo.com>
2021-04-08 23:59:37 +0300

    Entity Classes & Repository Interfaces Added

---
+++ CarRental_App/src/main/java/com/carrentalservice/entity/Car.java
@@ -0,0 +1,113 @@
+package com.carrentalservice.entity;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Car extends BaseEntity {
+
+    private String make;
+    private String model;
+
+    @Enumerated(EnumType.STRING)
+    private BodyType bodyType;
+
+    private int yearOfProduction;
+    private String color;
+    private int mileage;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    private Double amount;
+
+    private String urlOfImage;
+
+    public Car(String make, String model, BodyType bodyType, int yearOfProduction, String color, int mileage, Status status, Double amount, String urlOfImage) {
+        this.make = make;
+        this.model = model;
+        this.bodyType = bodyType;
+        this.yearOfProduction = yearOfProduction;
+        this.color = color;
+        this.mileage = mileage;
+        this.status = status;
+        this.amount = amount;
+        this.urlOfImage = urlOfImage;
+    }
+
+    public Car() {
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public BodyType getBodyType() {
+        return bodyType;
+    }
+
+    public void setBodyType(BodyType bodyType) {
+        this.bodyType = bodyType;
+    }
+
+    public int getYearOfProduction() {
+        return yearOfProduction;
+    }
+
+    public void setYearOfProduction(int yearOfProduction) {
+        this.yearOfProduction = yearOfProduction;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public String getUrlOfImage() {
+        return urlOfImage;
+    }
+
+    public void setUrlOfImage(String urlOfImage) {
+        this.urlOfImage = urlOfImage;
+    }
+}
---
+++ CarRental_App/src/main/java/com/carrentalservice/entity/User.java
@@ -0,0 +1,50 @@
+package com.carrentalservice.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class User extends BaseEntity {
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+
+    public User(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public User() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String userType) {
+        this.role = userType;
+    }
+}
+
