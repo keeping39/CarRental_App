b4fc5423525e87e6af2c2270d9c9f14490dd8e2a
Ovidiu Piorescu <djenergydj@yahoo.com>
2021-04-09 01:42:32 +0300

    Updates

---
+++ CarRental_App/src/main/java/com/carrentalservice/repository/BranchRepository.java
@@ -0,0 +1,13 @@
+package com.carrentalservice.repository;
+
+import com.carrentalservice.entity.Branch;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BranchRepository extends JpaRepository<Branch, Long> {
+
+}
+
