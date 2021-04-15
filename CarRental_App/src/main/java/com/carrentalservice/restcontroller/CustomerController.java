a3067934120a46b3f9e1529b22f71dd761583885
DESKTOP-2H30V0N\flori <ncflorin99@gmail.com>
2021-04-15 21:40:47 +0300

    RestController Class

---
+++ CarRental_App/src/main/java/com/carrentalservice/restcontroller/CustomerController.java
@@ -0,0 +1,67 @@
+package com.sda.carrentalservice.restcontroller;
+
+import com.sda.carrentalservice.dto.CustomerDTO;
+import com.sda.carrentalservice.entity.Customer;
+import com.sda.carrentalservice.service.CustomerService;
+import com.sda.carrentalservice.transformer.CustomerTransformer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api/customer")
+@CrossOrigin(origins = "*")
+public class CustomerController {
+
+    private final CustomerService customerService;
+    private final CustomerTransformer customerTransformer;
+
+    @Autowired
+    public CustomerController(CustomerService customerService, CustomerTransformer customerTransformer) {
+        this.customerService = customerService;
+        this.customerTransformer = customerTransformer;
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<CustomerDTO> findCustomerById(@PathVariable("id") Long id) {
+        Customer customer = customerService.findCustomerById(id);
+        CustomerDTO customerDTO = customerTransformer.transformFromEntityToDTO(customer);
+        return ResponseEntity.ok(customerDTO);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<CustomerDTO> deleteCustomerById(@PathVariable("id") Long id) {
+        customerService.deleteCustomerById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<CustomerDTO> createCustomer(@RequestBody CustomerDTO customerDTO) {
+        Customer customer = customerTransformer.transformFromDTOToEntity(customerDTO);
+        Customer saveCustomer = customerService.saveCustomer(customer);
+        CustomerDTO savedCustomerDTO = customerTransformer.transformFromEntityToDTO(saveCustomer);
+        return ResponseEntity.ok(savedCustomerDTO);
+    }
+
+    @PutMapping
+    public ResponseEntity<CustomerDTO> updateCustomer(@RequestBody CustomerDTO customerDTO) {
+        Customer customer = customerTransformer.transformFromDTOToEntity(customerDTO);
+        Customer saveCustomer = customerService.saveCustomer(customer);
+        CustomerDTO savedCustomerDTO = customerTransformer.transformFromEntityToDTO(saveCustomer);
+        return ResponseEntity.ok(savedCustomerDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CustomerDTO>> listAllCustomer(){
+        List<Customer> allCustomer = customerService.findAllCustomer();
+        List<CustomerDTO> allCustomerDTO = new ArrayList<>();
+
+        for (Customer customer: allCustomer){
+            allCustomerDTO.add(customerTransformer.transformFromEntityToDTO(customer));
+        }
+        return ResponseEntity.ok(allCustomerDTO);
+    }
+}
---
+++ CarRental_App/src/main/java/com/carrentalservice/restcontroller/EmployeeRestController.java
@@ -0,0 +1,67 @@
+package com.sda.carrentalservice.restcontroller;
+
+import com.sda.carrentalservice.dto.EmployeeDTO;
+import com.sda.carrentalservice.entity.Employee;
+import com.sda.carrentalservice.service.EmployeeService;
+import com.sda.carrentalservice.transformer.EmployeeTransformer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping(path = "/api/employee")
+public class EmployeeRestController {
+
+    private final EmployeeService employeeService;
+    private final EmployeeTransformer employeeTransformer;
+
+    @Autowired
+    public EmployeeRestController(EmployeeService employeeService, EmployeeTransformer employeeTransformer) {
+        this.employeeService = employeeService;
+        this.employeeTransformer = employeeTransformer;
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<EmployeeDTO> findEmployeeById(@PathVariable("id") Long id) {
+        Employee employee = employeeService.findEmployeeById(id);
+        EmployeeDTO employeeDTO = employeeTransformer.transformFromEntityToDTO(employee);
+        return ResponseEntity.ok(employeeDTO);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<EmployeeDTO> deleteEmployeeById(@PathVariable("id") Long id) {
+        employeeService.deleteEmployeeById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<EmployeeDTO> createEmployee(@RequestBody EmployeeDTO employeeDTO) {
+        Employee employee = employeeTransformer.transformFromDTOToEntity(employeeDTO);
+        Employee savedEmployee = employeeService.saveEmployee(employee);
+        EmployeeDTO savedEmployeeDTO = employeeTransformer.transformFromEntityToDTO(savedEmployee);
+        return ResponseEntity.ok(savedEmployeeDTO);
+    }
+
+    @PutMapping
+    public ResponseEntity<EmployeeDTO> updateEmployee(@RequestBody EmployeeDTO employeeDTO) {
+        Employee employee = employeeTransformer.transformFromDTOToEntity(employeeDTO);
+        Employee savedEmployee = employeeService.saveEmployee(employee);
+        EmployeeDTO savedEmployeeDTO = employeeTransformer.transformFromEntityToDTO(savedEmployee);
+        return ResponseEntity.ok(savedEmployeeDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<EmployeeDTO>> listAllEmployees() {
+        List<Employee> allEmployees = employeeService.findAllEmployees();
+        List<EmployeeDTO> allEmployeesDTO = new ArrayList<>();
+
+        for (Employee employee : allEmployees) {
+            allEmployeesDTO.add(employeeTransformer.transformFromEntityToDTO(employee));
+        }
+        return ResponseEntity.ok(allEmployeesDTO);
+    }
+}
