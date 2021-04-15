c98395c4314d70e2f1322a33bc5da65f08cf99f4
Munteanu Stefan <didicu39@gmail.com>
2021-04-15 21:20:22 +0300

    Transformers

---
+++ CarRental_App/src/main/java/com/carrentalservice/transformer/BookingTransfromer.java
@@ -0,0 +1,23 @@
+package com.sda.carrentalservice.transformer;
+
+import com.sda.carrentalservice.dto.BookingDTO;
+import com.sda.carrentalservice.entity.Booking;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BookingTransfromer {
+
+    public Booking transformFromDTOToEntity(BookingDTO bookingDTO){
+        Booking booking = new Booking();
+        BeanUtils.copyProperties(bookingDTO, booking);
+        return booking;
+    }
+
+    public BookingDTO transformFromEntityToDTO(Booking booking) {
+        BookingDTO bookingDTO = new BookingDTO();
+        BeanUtils.copyProperties(booking, bookingDTO);
+        return bookingDTO;
+    }
+
+}
---
+++ CarRental_App/src/main/java/com/carrentalservice/transformer/EmployeeTransformer.java
@@ -0,0 +1,22 @@
+package com.sda.carrentalservice.transformer;
+
+import com.sda.carrentalservice.dto.EmployeeDTO;
+import com.sda.carrentalservice.entity.Employee;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmployeeTransformer {
+
+    public EmployeeDTO transformFromEntityToDTO(Employee employee) {
+        EmployeeDTO employeeDTO = new EmployeeDTO();
+        BeanUtils.copyProperties(employee, employeeDTO);
+        return employeeDTO;
+    }
+
+    public Employee transformFromDTOToEntity(EmployeeDTO employeeDTO) {
+        Employee employee = new Employee();
+        BeanUtils.copyProperties(employeeDTO, employee);
+        return employee;
+    }
+}
